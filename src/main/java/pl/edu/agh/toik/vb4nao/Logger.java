ce4a9953277fb77e30ca8292a3dcd5b307365d6f
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-05-18 21:21:56 +0200

    first tests

---
+++ src/main/java/pl/edu/agh/toik/vb4nao/Logger.java
@@ -0,0 +1,24 @@
+package pl.edu.agh.toik.vb4nao;
+
+/**
+ * Created by lopiola on 18.05.14.
+ */
+public class Logger {
+    private String className;
+
+    public Logger(Class<?> _class) {
+        this.className = _class.getSimpleName();
+    }
+
+    public void debug(String message) {
+        System.out.println("[DEBUG] " + className+ ": " + message);
+    }
+
+    public void info(String message) {
+        System.out.println("[INFO] " + className+ ": " + message);
+    }
+
+    public void error(String message) {
+        System.out.println("[ERROR] " + className+ ": " + message);
+    }
+}
--- src/main/java/pl/edu/agh/toik/vb4nao/Test.java
+++ src/main/java/pl/edu/agh/toik/vb4nao/Test.java
@@ -4,7 +4,20 @@ package pl.edu.agh.toik.vb4nao;
  * Created by lopiola on 18.05.14.
  */
 public class Test {
+    Logger logger = new Logger(this.getClass());
+
+    private static String NAOQI_IP = "127.0.0.1";
+    private static int NAOQI_PORT = 9559;
+
+    public Test() {
+        logger.error("dupka");
+
+
+        ALTextToSpeechProxy tts = new ALTextToSpeechProxy(NAOQI_IP, NAOQI_PORT);
+        tts.say("Hello, world");
+    }
+
     public static void main(String[] args) {
-        System.out.println("dupka");
+        new Test();
     }
 }
