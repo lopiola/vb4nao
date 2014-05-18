c86aa2fbab660bfe0088fed4b077f48e4b118f9f
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-05-18 19:51:40 +0200

    tworze projekt w mvn

---
+++ src/main/java/pl/edu/agh/toik/vb4nao/Test.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.toik.vb4nao;
+
+/**
+ * Created by lopiola on 18.05.14.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("dupka");
+    }
+}
