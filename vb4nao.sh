27423cfe50159d0c1d22c7d1e0e2feb872c01520
jstypka <jasieek@student.agh.edu.pl>
2014-05-21 21:39:23 +0200

    Naprawa poprzedniego commita, przez przypadek commitowałem windowsową wersję skryptu uruchamiającego

--- vb4nao.sh
+++ vb4nao.sh
@@ -4,6 +4,6 @@ RUNNER_SCRIPT_DIR=$(cd ${0%/*} && pwd)
 
 cd $RUNNER_SCRIPT_DIR
 
-CP=`ls lib | grep jar | sed -e 's|^|lib/|' | tr '\n' ';'`
+CP=`ls lib | grep jar | sed -e 's|^|lib/|' | tr '\n' ':'`
 
 java -Djava.library.path=lib/ -cp $CP pl.edu.agh.toik.vb4nao.Application
